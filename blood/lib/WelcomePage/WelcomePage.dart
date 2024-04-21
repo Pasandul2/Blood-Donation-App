c0d77b93535c6c042528e7478dba72d852605ce0
Pasandul2 <pamithpasandul@gmail.com>
2024-04-21 10:52:21 +0530

    Welcome page add

---
+++ blood/lib/WelcomePage/WelcomePage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.width,
+        child: Stack(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 1.6,
+              decoration: BoxDecoration(color: Colors.white),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width / 1.6,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius:
+                    BorderRadius.only(bottomRight: Radius.circular(70)),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
