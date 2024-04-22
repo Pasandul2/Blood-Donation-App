84b08f2e8ce51a9a7bc8c36e72e8c282436c3714
Pasandul2 <pamithpasandul@gmail.com>
2024-04-22 22:08:18 +0530

    (welcome page edit)

--- blood/lib/WelcomePage/WelcomePage.dart
+++ blood/lib/WelcomePage/WelcomePage.dart
@@ -16,18 +16,60 @@ class _WelcomePageState extends State<WelcomePage> {
         height: MediaQuery.of(context).size.width,
         child: Stack(
           children: [
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 1.6,
-              decoration: BoxDecoration(color: Colors.white),
+            Stack(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width / 1.6,
+                  decoration: BoxDecoration(color: Colors.white),
+                ),
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.width / 1,
+                  decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius:
+                        BorderRadius.only(bottomRight: Radius.circular(70)),
+                  ),
+                  child: Center(
+                    child: Image.asset(
+                      "assets/image1.png",
+                      scale: 0.35,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Container(
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.width / 1.6,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius:
-                    BorderRadius.only(bottomRight: Radius.circular(70)),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.width / 1,
+                padding: EdgeInsets.only(top: 40, bottom: 30),
+                decoration: BoxDecoration(
+                    color: Colors.red,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(70),
+                    )),
+                child: Column(
+                  children: [
+                    Text(
+                      "Blood Donation Application",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1,
+                        wordSpacing: 2,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 40),
+                      child: Text("aaaaaaaaaaaaaaaaaaaaaaa"),
+                    )
+                  ],
+                ),
               ),
             )
           ],
