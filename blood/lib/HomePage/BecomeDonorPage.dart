a2bb3740e1ff78d162beb043ec74b17328365b2b
Sewmini Maddumaarachchi <sewminimaddumaarachchi@gmail.com>
2024-05-31 23:31:06 +0530

    updated

--- blood/lib/HomePage/BecomeDonorPage.dart
+++ blood/lib/HomePage/BecomeDonorPage.dart
@@ -47,7 +47,7 @@ class BecomeDonorPage extends StatelessWidget {
             // Map functionality can be added here
           } else if (index == 2) {
             showLogoutConfirmationDialog(
-                context); // Show logout confirmation dialog
+                 // Show logout confirmation dialog
           }
         },
       ),
